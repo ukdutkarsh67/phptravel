f3bdb8590bd849e3a702ab0fe935b5d97c5510c3
UTKARSH DWIVEDI <utkarsh.dwivedi@happiestminds.com>
2024-04-11 11:35:13 +0530

    initial

--- test/pageobjects/cartPage.ts
+++ test/pageobjects/cartPage.ts
@@ -1,67 +1,66 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
 
-export default class CartPage extends Page{
-    get  cartItem() { 
+export default class CartPage extends Page {
+    get cartItem() {
         return $('//div[@class="product-description"]/div[1]')
     };
 
-    get ProductPrice(){
+    get ProductPrice() {
         return $('[class="product-price"]');
     }
 
-    get productTaxPrice(){
+    get productTaxPrice() {
         return $('//div[@class="lines"]/div[@class="line"][2]/div[2]');
     }
 
-    get productShippingPrice(){
+    get productShippingPrice() {
         return $('//div[@class="lines"]/div[@class="line"][3]/div[2]');
     }
 
-    get totalPrice(){
+    get totalPrice() {
         return $('//div[@class="line line-total"]/div[2]');
     }
 
-    get checkoutButton(){
+    get checkoutButton() {
         return $('[class="checkout"]');
     }
 
 
-    get orderPlaced(){
+    get orderPlaced() {
         return $('[class="checkout-title"]');
     }
 
 
-    async verifyProductAddedToCart(productName:string){
-        const productText=await this.cartItem.getText();
+    async verifyProductAddedToCart(productName: string) {
+        const productText = await this.cartItem.getText();
         await expect(productText).toContain(productName);
     }
 
 
-    async validatePriceOfProduct(productPrice:number){
-        const productText=await this.ProductPrice.getText();
-        const price=productText.split('$');
+    async validatePriceOfProduct(productPrice: number) {
+        const productText = await this.ProductPrice.getText();
+        const price = productText.split('$');
         await expect(+price[1]).toBe(productPrice);
     }
 
-    async validateTotalPriceOfCart(productPrice:number){
-        const taxText=await  this.productTaxPrice.getText();
-        const  shippingText = await this.productShippingPrice.getText();
-        const totalText=await this.totalPrice.getText();
-        const taxPrice=+taxText;
-        const shippingPrice=+shippingText;
-        const totalPrice=+totalText;
-        const totalAmount=taxPrice+shippingPrice+productPrice;
+    async validateTotalPriceOfCart(productPrice: number) {
+        const taxText = await this.productTaxPrice.getText();
+        const shippingText = await this.productShippingPrice.getText();
+        const totalText = await this.totalPrice.getText();
+        const taxPrice = +taxText;
+        const shippingPrice = +shippingText;
+        const totalPrice = +totalText;
+        const totalAmount = taxPrice + shippingPrice + productPrice;
         await expect(totalPrice).toBe(totalAmount);
     }
 
 
-    async clickCheckoutButton(){
+    async clickCheckoutButton() {
         await this.checkoutButton.click();
     }
 
-    async isOrderPlaced(){
-        
+    async isOrderPlaced() {
         return await this.orderPlaced.isDisplayed();
     }
 
--- test/pageobjects/homePage.ts
+++ test/pageobjects/homePage.ts
@@ -1,27 +1,27 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
 
-export default class HomePage extends Page{
-    get  pageHeading() { 
+export default class HomePage extends Page {
+    get pageHeading() {
         return $('.brand-small img');
     }
 
-    get productList(){
+    get productList() {
         return $$(
             '[class="menu-item-large"]'
-            );
+        );
     }
 
-    public async selectProductPage(productPage:any){
+    public async selectProductPage(productPage: any) {
         const productsLink = await this.productList;
-        const countProductPage= productsLink.length;
+        const countProductPage = productsLink.length;
         console.log(countProductPage);
 
-        for(let i=0;i<countProductPage;i++){
-            const text=await productsLink[i].getText();
-            console.log('##########33',text);
-            if(text.includes(productPage)){
-                console.log('##########',text);
+        for (let i = 0; i < countProductPage; i++) {
+            const text = await productsLink[i].getText();
+            console.log('##########33', text);
+            if (text.includes(productPage)) {
+                console.log('##########', text);
                 await productsLink[i].waitForClickable();
                 await productsLink[i].click();
                 break;
@@ -29,7 +29,7 @@ export default class HomePage extends Page{
 
         }
     }
-    public async isHeaderLinkOpen(linkText: string){
+    public async isHeaderLinkOpen(linkText: string) {
         await expect(browser).toHaveUrl(expect.stringContaining(linkText));
     }
 
